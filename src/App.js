967e15e05e40c0fcbe9f49a077dda795bb84d797
Raman Sharma <rs9463194297@gmail.com>
2021-07-15 10:03:23 +0530

    added undo redo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { useRef, useState } from 'react';
-import './style.css';
+import React, { useRef, useState } from "react";
+import "./style.css";
 
-import Canvas from './Canvas';
+import Canvas from "./Canvas";
 
 const App = () => {
   const canvasRef = useRef(null);
   const ctx = useRef(null);
-  const [color, setColor] = useState('#000000');
-  const [elements,setElements] = useState([]);
-  const [tool,setTool] = useState("rect");
+  const [color, setColor] = useState("#000000");
+  const [elements, setElements] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [tool, setTool] = useState("pencil");
 
   const clearCanvas = () => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.fillStyle = 'white';
+    const context = canvas.getContext("2d");
+    context.fillStyle = "white";
     context.fillRect(0, 0, canvas.width, canvas.height);
     setElements([]);
+  };
+
+  const undo = () => {
+    setHistory((prevHistory) => [
+      ...prevHistory,
+      elements[elements.length - 1],
+    ]);
+    setElements((prevElements) =>
+      prevElements.filter((ele, index) => index !== elements.length - 1)
+    );
+  };
+  const redo = () => {
+    setElements((prevElements) => [
+      ...prevElements,
+      history[history.length - 1],
+    ]);
+    setHistory((prevHistory) =>
+      prevHistory.filter((ele, index) => index !== history.length - 1)
+    );
   };
   return (
     <div className="container-fluid">
@@ -29,24 +49,76 @@ const App = () => {
             <input
               type="color"
               value={color}
-              onChange={e => setColor(e.target.value)}
+              onChange={(e) => setColor(e.target.value)}
             />
           </div>
         </div>
         <div className="col-md-3">
-           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="tools" id="pencil" value="pencil"  checked={tool === "pencil"} onClick={(e)=> setTool(e.target.value)} />
-            <label className="form-check-label" htmlFor="pencil">Pencil</label>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="tools"
+              id="pencil"
+              value="pencil"
+              checked={tool === "pencil"}
+              onClick={(e) => setTool(e.target.value)}
+              readOnly={true}
+            />
+            <label className="form-check-label" htmlFor="pencil">
+              Pencil
+            </label>
           </div>
           <div className="form-check form-check-inline">
-           
-            <input className="form-check-input" type="radio" name="tools" id="line" value="line" checked={tool === "line"} onClick={(e)=> setTool(e.target.value)} />
-            <label className="form-check-label" htmlFor="line">Line</label>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="tools"
+              id="line"
+              value="line"
+              checked={tool === "line"}
+              onClick={(e) => setTool(e.target.value)}
+              readOnly={true}
+            />
+            <label className="form-check-label" htmlFor="line">
+              Line
+            </label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="tools" id="rect" value="rect"  checked={tool === "rect"} onClick={(e)=> setTool(e.target.value)} />
-            <label className="form-check-label" htmlFor="rect">Rectangle</label>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="tools"
+              id="rect"
+              value="rect"
+              checked={tool === "rect"}
+              onClick={(e) => setTool(e.target.value)}
+              readOnly={true}
+            />
+            <label className="form-check-label" htmlFor="rect">
+              Rectangle
+            </label>
           </div>
+        </div>
+
+        <div className="col-md-2">
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            disabled={elements.length === 0}
+            onClick={() => undo()}
+          >
+            Undo
+          </button>
+          &nbsp;&nbsp;
+          <button
+            type="button"
+            className="btn btn-outline-primary ml-1"
+            disabled={history.length < 1}
+            onClick={() => redo()}
+          >
+            Redo
+          </button>
         </div>
         <div className="col-md-1">
           <div className="color-picker d-flex align-items-center justify-content-center">
@@ -60,7 +132,14 @@ const App = () => {
         </div>
       </div>
       <div className="row">
-        <Canvas canvasRef={canvasRef} ctx={ctx} color={color} setElements={setElements} elements={elements} tool={tool} />
+        <Canvas
+          canvasRef={canvasRef}
+          ctx={ctx}
+          color={color}
+          setElements={setElements}
+          elements={elements}
+          tool={tool}
+        />
       </div>
     </div>
   );
